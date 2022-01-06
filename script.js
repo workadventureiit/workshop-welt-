fc2fa3dab8f1221feb97b2d3592e8b5a9cead596
workadventureiit <93705123+workadventureiit@users.noreply.github.com>
2022-01-06 22:55:18 +0100

    update

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-let helloWorldPopup;
 
-// Open the popup when we enter a given zone
-helloWorldPopup = WA.room.onEnterLayer("myZone").subscribe(() => {
-    WA.ui.openPopup("PopUp", 'Guten Tag', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    }]);
-});
-
-// Close the popup when we leave the zone.
-WA.room.onLeaveLayer("myZone").subscribe(() => {
-    helloWorldPopup.close();
-})
