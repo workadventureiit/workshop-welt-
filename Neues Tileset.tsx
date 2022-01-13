083adee303d1b52b77a668752b5dbfda7f8e557a
workadventureiit <93705123+workadventureiit@users.noreply.github.com>
2022-01-13 16:36:11 +0100

    update

---
+++ Neues Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Mega32x32PixelArtIconsSpriteSheet_Cover" tilewidth="32" tileheight="32" tilecount="1254" columns="22">
+ <image source="src/assets/Mega32x32PixelArtIconsSpriteSheet_Cover.jpg" width="708" height="1825"/>
+</tileset>
