1ce14fb6e7157f0bf41bd07a040e2fb76606d8f4
workadventureiit <93705123+workadventureiit@users.noreply.github.com>
2022-01-18 10:23:32 +0100

    up

--- MegaPixelArt32x32pxIcons_SpriteSheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="MegaPixelArt32x32pxIcons_SpriteSheet" tilewidth="32" tileheight="32" tilecount="504" columns="14">
- <image source="src/assets/MegaPixelArt32x32pxIcons_SpriteSheet.png" width="448" height="1152"/>
-</tileset>
